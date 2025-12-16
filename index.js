9609a938ade15792bb219978b9106906cd162305
tareadelau0-blip <tareadelau0@gmail.com>
2025-12-16 13:18:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
+app.get("/", (req, res) => {
+  res.send("Sistema de pagos en línea funcionando ✅");
+});
 const app = express();
 app.use(express.json());
 
